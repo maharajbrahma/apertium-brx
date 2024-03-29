d07db403739d033690a99332e0d85da8ddcd6411
Maharaj Brahma <mraj.brahma@gmail.com>
2024-03-29 12:12:28 +0530

    Initial commit

---
+++ compile.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+lexd apertium-brx.brx.lexd > apertium-brx.brx.att
+
+lt-comp rl apertium-brx.brx.att apertium-brx.brx.bin
---
+++ test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Test Words
+cat test/analysis-main.txt | lt-proc apertium-brx.brx.bin > test/analysis-output-main.txt
+
+# Test Nouns
+cat test/analysis-input-noun.txt | lt-proc apertium-brx.brx.bin > test/analysis-output-noun.txt
+
+# Test Verbs
+cat test/analysis-input-verb.txt | lt-proc apertium-brx.brx.bin >test/analysis-output-verb.txt
